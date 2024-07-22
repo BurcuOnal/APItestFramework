ad2a40c9c7c4d762ff6c67c851ece8f82b72c5a5
burcuonal <burcu.onal@testinium.com>
2024-07-22 15:58:38 +0300

    data generator seperated from utilities

--- src/test/java/util/Utilities.java
+++ src/test/java/util/Utilities.java
@@ -10,25 +10,7 @@ public class Utilities {
     }
 
 
-    public static String generateRandomUsername() {
-        return "user_" + System.currentTimeMillis();
-    }
 
-    public static String generateRandomEmail() {
-        return "user_" + System.currentTimeMillis() + "@example.com";
-    }
-    /* these helpers generate random username and email if needed
-    how to use them in the karate scenario:
-    ------->
-    Background:
-  * def Utilities = Java.type('utilities.Utilities')
-
-    Scenario: Yeni kullanıcı oluşturma
-      Given path '/users'
-      And request { username: '#(Utilities.generateRandomUsername())', email: '#(Utilities.generateRandomEmail())' }
-      When method POST
-      Then status 200
-     */
 }
 
 
